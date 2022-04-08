d766d6e65aa106e460a5227fabaf92a033c46b54
lamnt95 <tunglam20132208@gmail.com>
2022-04-08 15:29:45 +0700

    3 files modified, 1 added

--- index.js
+++ index.js
@@ -3,6 +3,16 @@ const app = express();
 const port = 3010;
 const path = require('path');
 
+var admin = require('firebase-admin');
+
+var serviceAccount = require('./serviceAccountKey.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL:
+    'https://cryptoogateway-default-rtdb.asia-southeast1.firebasedatabase.app',
+});
+
 app.use(express.static('static'));
 
 app.get('/', (req, res) => {
